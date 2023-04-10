ab9dd34e9de260a5f29ff797ffa6ba356a765155
Gabriel Bach <gabrielcassolbach@gmail.com>
2023-04-10 13:17:47 -0300

    Libraries - Mark 1

--- FasTicket/src/main.cpp
+++ FasTicket/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <Adafruit_BusIO_Register.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <TTP229.h>
+
+// For the Adafruit shield, these are the default.
+#define TFT_DC 9
+#define TFT_CS 10
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  tft.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+
+
+
 }
